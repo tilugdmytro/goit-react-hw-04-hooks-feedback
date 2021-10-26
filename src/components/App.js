120894f9d160aa100c78c47d6f79c421a4870a18
Dmytro Tilug <tilugdmytro@gmail.com>
2021-10-26 16:37:02 +0300

    рефакторинг App на хуки

--- src/components/App.js
+++ src/components/App.js
@@ -21,13 +21,13 @@ function App() {
   const onFeedbackClick = (option) => {
     switch (option) {
       case "good":
-        setGood((prevState) => prevState + 1);
+        setGood((state) => state + 1);
         break;
       case "neutral":
-        setNeutral((prevState) => prevState + 1);
+        setNeutral((state) => state + 1);
         break;
       case "bad":
-        setBad((prevState) => prevState + 1);
+        setBad((state) => state + 1);
         break;
 
       default:
